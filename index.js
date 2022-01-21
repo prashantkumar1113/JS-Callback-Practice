91e1525a6c144df544f96b9c3ed92fc3b0da2b3f
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-01-22 00:17:21 +0530

    Added the moveArrowKeys func to move.js

--- index.js
+++ index.js
@@ -2,29 +2,7 @@ const inventory = newInventory();
 move(inventory).to(0, 0);
 
 const character = newImage("assets/green-character/static.gif");
-let direction = null;
-let x = 100;
-let y = 250;
-
-function moveCharacter() {
-    if (direction === "west") {
-        x = x - 1;
-    }
-    if (direction === "north") {
-        y = y + 1;
-    }
-    if (direction === "east") {
-        x = x + 1;
-    }
-    if (direction === "south") {
-        y = y - 1;
-    }
-    character.style.left = x + "px";
-    character.style.bottom = y + "px";
-    move(character).to(x, y);
-}
-// move(character).to(x, y);
-setInterval(moveCharacter, 1);
+move(character).withArrowKeys(200, 450);
 
 move(newImage("assets/tree.png")).to(200, 450);
 move(newImage("assets/pillar.png")).to(350, 250);
